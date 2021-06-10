95282e299059e53b627eb5c5426413277036cb77
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-06-10 01:06:16 -0400

    Completed assignment

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <NavBar />
+      <div>
+        <Route path="/" component={Home} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/movies" component={Movies} />
+      </div>
     </Router>
   );
 };
